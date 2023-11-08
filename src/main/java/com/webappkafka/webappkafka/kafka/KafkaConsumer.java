8de5140b0b563bd9b7c54a5e59e2585d7858966e
uday-kiran-k <113407692+uday-kiran-k@users.noreply.github.com>
2023-11-08 02:02:47 -0500

    fix:check created kafka

--- src/main/java/com/webappkafka/webappkafka/kafka/KafkaConsumer.java
+++ src/main/java/com/webappkafka/webappkafka/kafka/KafkaConsumer.java
@@ -1,5 +1,8 @@
 package com.webappkafka.webappkafka.kafka;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,8 @@ public class KafkaConsumer {
     @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(HealthCheck healthCheck){
         LOGGER.info(String.format("Json message recieved -> %s", healthCheck.toString()));
+        healthCheck.setCheck_created(OffsetDateTime.now(ZoneOffset.UTC).toString());
+        healthCheck.setCheck_updated(OffsetDateTime.now(ZoneOffset.UTC).toString());
         healthCheckRepo.save(healthCheck);
     }
 }
